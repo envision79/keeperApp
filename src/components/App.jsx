c43a779517185380b8a33e5a309ac2ec9dad1614
Adarsh Kumar <kumar.adarsh0042@gmail.com>
2021-10-05 23:58:10 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+/* class can't be added in custom components (only on html elements)
+because inside of a custom component it sees each of them as custom property(props)*/
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        key: 0,
+        title: "This is the title",
+        content: "This is the content",
+    },
+    {
+        key: 1,
+        title: "This is the title",
+        content: "This is the content",
+    },
+    {
+        key: 2,
+        title: "This is the title",
+        content: "This is the content",
+    },
+    {
+        key: 3,
+        title: "This is the title",
+        content: "This is the content",
+    },
+];
+
+export default notes;
